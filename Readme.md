85cee0714d82d72182a2f6efe99671afd02da0e2
Tobias <tobias@raqt.com>
2019-03-18 14:03:19 +0100

    First checkin for TFS Iterations. This tools updates iterations on teams automatically.

---
+++ Readme.md
@@ -0,0 +1,44 @@
+﻿#Introduction 
+Add iterations to teams in config automatically. 
+
+#Getting Started
+1. Create a personal token in Azure DevOps or TFS
+2. Update appsettings.json file with you local settings.
+
+        {
+        "baseUrl": "http://tfs.Server.com:8080/",
+        "collection": "DefaultCollection",
+        "project": "Your project",
+        "teams": [
+            "DevOps",
+            "Core",
+            "Scheduling",
+            "Product",
+            "Test",
+            "Project Management"
+        ],
+        "iterationToAddToTeams": {
+            "test/2021 August": {
+            "attributes": {
+                "startDate": "2021-10-15",
+                "finishDate": "2022-10-31"
+            }
+            },
+            "test/2019 Oktober": {
+            "attributes": {
+                "startDate": "2018-11-15",
+                "finishDate": "2018-11-31"
+            }
+            }
+        },
+        "personalAccessToken": "wdgcdtvlhxsrbg6tcnzrphdlbc3sy2lldiehhtggrbolhz2zlqda"
+
+        }
+
+
+#Build and Test
+Run test in Visual Studio and it will add and set active iterations on each team.
+
+#Contribute
+- Check if it works on Azure DevOps
+- Add support for removing sprints from teams
---
+++ TfsIterations/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace TfsIterations
+{
+    class Program
+    {
+        internal static IConfiguration Configuration { get; set; }
+
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            TfsVariables.Teams = Configuration.GetSection("teams").GetChildren().ToList();
+            TfsVariables.Collection = Configuration["collection"];
+            TfsVariables.BaseUrl = Configuration["baseUrl"];
+            TfsVariables.TeamDefaultBacklogIteration = Configuration["teamDefaultBacklogIteration"];
+            TfsVariables.Project = Configuration["project"];
+            TfsVariables.PersonalAccessToken = Configuration["personalAccessToken"];
+            TfsVariables.IterationToAddToTeams = Configuration.GetSection("iterationToAddToTeams").GetChildren().ToList();
+
+            //RunAsync().GetAwaiter().GetResult();
+            RunIterationAddToTeams().GetAwaiter().GetResult();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        //static async Task RunAsync()
+        //{
+            
+        //    //var x = await TfsApi.GetProjects();
+
+        //}
+        static async Task RunIterationAddToTeams()
+        {           
+            await AddIterationToTeams.CreateIteration(Configuration);
+
+        }
+    }
+
+
+    public class TfsVariables
+    {
+        public static string Collection { get; set; }
+        public static List<IConfigurationSection> Teams { get; set; }
+        public static string BaseUrl { get; internal set; }
+        public static string Project { get; internal set; }
+        public static string PersonalAccessToken { get; set; }
+        public static List<IConfigurationSection> IterationToAddToTeams { get; set; }
+
+        public static string TeamDefaultBacklogIteration { get; set; }
+
+    }
+}
